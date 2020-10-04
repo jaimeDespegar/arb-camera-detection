213f70e1b1434107f038296c4704fa3fb7e75764
jaimeDespegar <jaime.quispe.1994@outlook.com>
2020-10-04 00:03:44 -0300

    refactor

--- arb/motion_detector.py
+++ arb/motion_detector.py
@@ -20,36 +20,9 @@ class MotionDetector:
     def detect_motion(self):
         capture = open_cv.VideoCapture(self.video)
         capture.set(open_cv.CAP_PROP_POS_FRAMES, self.start_frame)
-
         coordinates_data = self.coordinates_data
-        logging.debug("coordinates data: %s", coordinates_data)
 
-        for p in coordinates_data:
-            coordinates = self._coordinates(p)
-            logging.debug("coordinates: %s", coordinates)
-
-            rect = open_cv.boundingRect(coordinates)
-            logging.debug("rect: %s", rect)
-
-            new_coordinates = coordinates.copy()
-            new_coordinates[:, 0] = coordinates[:, 0] - rect[0]
-            new_coordinates[:, 1] = coordinates[:, 1] - rect[1]
-            logging.debug("new_coordinates: %s", new_coordinates)
-
-            self.contours.append(coordinates)
-            self.bounds.append(rect)
-
-            mask = open_cv.drawContours(
-                np.zeros((rect[3], rect[2]), dtype=np.uint8),
-                [new_coordinates],
-                contourIdx=-1,
-                color=255,
-                thickness=-1,
-                lineType=open_cv.LINE_8)
-
-            mask = mask == 255
-            self.mask.append(mask)
-            logging.debug("mask: %s", self.mask)
+        self.calculateMask(coordinates_data)
 
         statuses = [False] * len(coordinates_data)
         times = [None] * len(coordinates_data)
@@ -65,38 +38,67 @@ class MotionDetector:
             blurred = open_cv.GaussianBlur(frame.copy(), (5, 5), 3)
             grayed = open_cv.cvtColor(blurred, open_cv.COLOR_BGR2GRAY)
             new_frame = frame.copy()
-            logging.debug("new_frame: %s", new_frame)
 
-            position_in_seconds = capture.get(open_cv.CAP_PROP_POS_MSEC) / 1000.0
-
-            for index, c in enumerate(coordinates_data):
-                status = self.__apply(grayed, index, c)
-
-                if times[index] is not None and self.same_status(statuses, index, status):
-                    times[index] = None
-                    continue
-
-                if times[index] is not None and self.status_changed(statuses, index, status):
-                    if position_in_seconds - times[index] >= MotionDetector.DETECT_DELAY:
-                        statuses[index] = status
-                        times[index] = None
-                    continue
-
-                if times[index] is None and self.status_changed(statuses, index, status):
-                    times[index] = position_in_seconds
-
-            for index, p in enumerate(coordinates_data):
-                coordinates = self._coordinates(p)
-
-                color = COLOR_GREEN if statuses[index] else COLOR_BLUE
-                draw_contours(new_frame, coordinates, str(p["id"] + 1), COLOR_WHITE, color)
+            self.calculateTimesByStatus(capture, grayed, times, statuses)
+            self.drawContoursInFrame(new_frame, statuses)
 
             open_cv.imshow(str(self.video), new_frame)
-            k = open_cv.waitKey(1)
+            k = open_cv.waitKey(10)
             if k == KEY_QUIT:
                 break
         capture.release()
         open_cv.destroyAllWindows()
+
+    # ver nombre
+    def calculateMask(self, coordinates_data):
+        for p in coordinates_data:
+            coordinates = self._coordinates(p)
+            rect = open_cv.boundingRect(coordinates)
+
+            new_coordinates = coordinates.copy()
+            new_coordinates[:, 0] = coordinates[:, 0] - rect[0]
+            new_coordinates[:, 1] = coordinates[:, 1] - rect[1]
+
+            self.contours.append(coordinates)
+            self.bounds.append(rect)
+
+            mask = open_cv.drawContours(
+                np.zeros((rect[3], rect[2]), dtype=np.uint8),
+                [new_coordinates],
+                contourIdx=-1,
+                color=255,
+                thickness=-1,
+                lineType=open_cv.LINE_8)
+
+            mask = mask == 255
+            self.mask.append(mask)        
+
+    # ver nombre
+    def calculateTimesByStatus(self, capture, grayed, times, statuses):
+        position_in_seconds = capture.get(open_cv.CAP_PROP_POS_MSEC) / 1000.0
+
+        for index, c in enumerate(self.coordinates_data):
+            status = self.__apply(grayed, index, c)
+            timesIsNone = times[index] is None
+
+            if not timesIsNone and self.same_status(statuses, index, status):
+                times[index] = None
+                continue
+
+            if not timesIsNone and self.status_changed(statuses, index, status):
+                if position_in_seconds - times[index] >= MotionDetector.DETECT_DELAY:
+                    statuses[index] = status
+                    times[index] = None
+                continue
+
+            if timesIsNone and self.status_changed(statuses, index, status):
+                times[index] = position_in_seconds
+        
+    def drawContoursInFrame(self, newFrame, statuses):
+        for index, p in enumerate(self.coordinates_data):
+            coordinates = self._coordinates(p)
+            color = COLOR_GREEN if statuses[index] else COLOR_BLUE
+            draw_contours(newFrame, coordinates, str(p["id"] + 1), COLOR_WHITE, color)        
 
     def __apply(self, grayed, index, p):
         coordinates = self._coordinates(p)
